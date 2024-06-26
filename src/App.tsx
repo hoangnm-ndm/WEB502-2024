55b8539d1317eb39309f35401e0ac45e97c4ddf6
hoangnm-ndm <hoangnm.ndm@gmail.com>
2024-06-26 14:13:58 +0700

    BTVN

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import "./App.css";
 
-interface Product {
-	id?: number;
-	name: string;
-	price: number;
-	description?: string;
-}
-
-const product1: Product = {
-	name: "iphone 11",
-	price: 400,
-	description: "mo ta chiec iphone",
-};
-
 function App() {
 	return (
 		<>
-			<p>
-				{product1.name} - {product1.price}
-			</p>
+			<div></div>
 		</>
 	);
 }
