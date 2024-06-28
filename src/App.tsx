ab16c342922765ffed31136a5d936831c43db649
hoangnm-ndm <hoangnm.ndm@gmail.com>
2024-06-28 12:12:11 +0700

    update btvn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import "./App.css";
 
 function App() {
+	// viet logic de lay du lieu
+
 	return (
 		<>
-			<div></div>
+			<table className="table table-bordered table-striped">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Title</th>
+						<th>Price</th>
+						<th>Thumbnail</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					{
+						// mapping du lieu vao giao dien
+					}
+				</tbody>
+			</table>
 		</>
 	);
 }
