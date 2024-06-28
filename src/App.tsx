e78d169347b84f64249b5f6104551f390aee049b
hoangnm-ndm <hoangnm.ndm@gmail.com>
2024-06-28 12:48:49 +0700

    Bai 3: table

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { Product } from "./interfaces/Product";
 
 function App() {
+	const [products, setProducts] = useState<Product[]>([]);
 	// viet logic de lay du lieu
+
+	// Promise cần thời gian để trả về.
+	useEffect(() => {
+		fetch("http://localhost:3000/products")
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				setProducts(data);
+			})
+			.catch((error) => console.log(error));
+	}, []);
+
+	// state = trạng thái
 
 	return (
 		<>
@@ -11,6 +27,7 @@ function App() {
 						<th>ID</th>
 						<th>Title</th>
 						<th>Price</th>
+						<th>Description</th>
 						<th>Thumbnail</th>
 						<th>Action</th>
 					</tr>
@@ -19,6 +36,21 @@ function App() {
 				<tbody>
 					{
 						// mapping du lieu vao giao dien
+						products.map((item) => (
+							<tr key={item.id}>
+								<td>{item.id}</td>
+								<td>{item.title}</td>
+								<td>{item.price}</td>
+								<td>{item.description}</td>
+								<td>
+									<img src={item.thumbnail} alt={item.title} width={100} />
+								</td>
+								<td>
+									<button className="btn btn-warning">Edit</button>
+									<button className="btn btn-danger">Remove</button>
+								</td>
+							</tr>
+						))
 					}
 				</tbody>
 			</table>
